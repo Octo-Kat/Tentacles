ea2c62bd38160e0e7031d38e546489f7be30d701
MassStash <MassStash@gmail.com>
2014-02-08 18:31:05 -0600

    [2/2] OmniGears: Quick swipe Change-Id: I2343fca2ef442b69f2238ede5b9a932cac2ad361
    Thanks OmniRom=(https://github.com/omnirom/android_packages_apps_OmniGears/commit/915b85f33dfad57b5385c444f55e9eca3a59b43a)

---
+++ src/org/oct/tentacles/notification/NotificationPanelSettings.java
@@ -0,0 +1,75 @@
+/*
+ *  Copyright (C) 2013 The OmniROM Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.oct.tentacles.notification;
+
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.R;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceGroup;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.util.Log;
+
+import org.oct.tentacles.R;
+
+public class NotificationPanelSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    private static final String TAG = "NotificationPanelSettings";
+
+    private static final String QUICK_SWIPE = "quick_swipe";
+
+    private CheckBoxPreference mQuickSwipe;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.notification_drawer);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mQuickSwipe = (CheckBoxPreference) prefSet.findPreference(QUICK_SWIPE);
+        mQuickSwipe.setChecked(Settings.System.getInt(resolver,
+            Settings.System.QUICK_SWIPE, 1) == 1);
+        mQuickSwipe.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        return true;
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mQuickSwipe) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(resolver,
+                Settings.System.QUICK_SWIPE, value ? 1 : 0);
+        } else {
+            return false;
+        }
+        return true;
+    }
+}
